a205fca2b10dfc3d5310090472b0c92b2e0bfa56
vhegde <vhegde@uw.edu>
2022-12-25 12:49:48 -0800

    Semi completed database and added three API endpoints

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const sqlite3 = require('sqlite3');
 const sqlite = require('sqlite');
 
 const INVALID_PARAM_ERROR = 400;
-const INVALID_PARAM_ERROR_MSG = "Missing one or more of the required params..";
+const INVALID_PARAM_ERROR_MSG = "Invalid parameters. Please try again";
 const SERVER_ERROR = 500;
 const SERVER_ERROR_MSG = "An error occurred on the server. Try again later.";
 
@@ -30,6 +30,89 @@ const SERVER_ERROR_MSG = "An error occurred on the server. Try again later.";
  * remmber to "let db = await getDBConnection();" and "db.close();"
  * remember whenever you are using user input for database use placeholders (?)
  */
+
+// This endpoint handles:
+// get all books
+// get books with specific genre
+app.get("/books/genre/:genre", async (req, res) => {
+  try {
+    let genre = req.params.genre;
+    let db = await getDBConnection();
+    let queryText = "SElECT id, Genre, Title, Author, Price, Stock " +
+    "FROM books";
+    let extra = " WHERE Genre LIKE ?;"
+    let result;
+    if (genre === "all") {
+      result = await db.all(queryText);
+    } else {
+      queryText += extra;
+      result = await db.all(queryText, "%" + genre + "%");
+    }
+    if (result.length === 0) {
+      res.status(INVALID_PARAM_ERROR);
+      res.type("text").send(INVALID_PARAM_ERROR_MSG);
+    } else {
+      res.json({
+        "books": result
+      });
+    }
+
+    db.close();
+  } catch (error) {
+    res.status(SERVER_ERROR);
+    res.type("text").send(SERVER_ERROR_MSG);
+  }
+});
+
+// This endpoint handles:
+// returns all titles with the search keyword in it regardless of genre
+app.get("/books/search/:title", async (req, res) => {
+  try {
+    let title = req.params.title;
+    let db = await getDBConnection();
+    let queryText = "SElECT id, Genre, Title, Author, Price, Stock " +
+    "FROM books WHERE Title LIKE ?;";
+
+    let result = await db.all(queryText, "%" + title + "%");
+
+    if (result.length === 0) {
+      res.type("text").send("No Books Found :(");
+    } else {
+      res.json({
+        "books": result
+      });
+    }
+
+    db.close();
+  } catch (error) {
+    res.status(SERVER_ERROR);
+    res.type("text").send(SERVER_ERROR_MSG);
+  }
+});
+
+// This endpoint returns all distinct genres in the database
+app.get("/books/all_genres", async (req, res) => {
+  try {
+    let db = await getDBConnection();
+    let queryText = "SElECT DISTINCT Genre FROM books";
+
+    let result = await db.all(queryText);
+
+    if (result.length === 0) {
+      res.status(SERVER_ERROR);
+      res.type("text").send(SERVER_ERROR_MSG);
+    } else {
+      res.json({
+        "books": result
+      });
+    }
+
+    db.close();
+  } catch (error) {
+    res.status(SERVER_ERROR);
+    res.type("text").send(SERVER_ERROR_MSG);
+  }
+});
 
 /**
  * Establishes a database connection to the database and returns the database object.
